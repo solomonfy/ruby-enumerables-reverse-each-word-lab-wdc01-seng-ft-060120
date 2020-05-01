e74c2b1f391b346c3f0d6c8d4c5fc70adda811e3
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-01 16:10:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word (string)
   
   string_array = string.split
   
-  string_array.each do |element|
-    element.reverse
-    
-  end
+string_array.collect do |element|
+    "#{element.reverse}"
+     end.join(" ")
+   
 end
